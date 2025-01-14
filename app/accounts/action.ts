0a868db00aab6272f0a85fcca438a0736b696882
Nehir Cesmeci <nehir.cesmeci@protonmail.com>
2025-01-14 12:16:29 +0100

    fix:actions

--- app/accounts/action.ts
+++ app/accounts/action.ts
@@ -2,7 +2,15 @@
 
 export async function getAccounts() {
   const url = new URL(process.env.ACCOUNT_MANAGER_API_URL + "/accounts");
-  const response = await fetch(url.toString());
-  const accounts = await response.json();
-  return accounts;
+  try {
+    const response = await fetch(url.toString());
+    console.log("accoouuuunts",response);
+    if(!response.ok){
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Failed to fetch accounts:", error);
+    return [];
+  }
 }
